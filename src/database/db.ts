dfc5b471da028e64b633cbbd2b897228670ebbd8
jime <jimemiramontes@gmail.com>
2024-10-23 21:29:16 -0300

    First commit

---
+++ src/database/db.ts
@@ -0,0 +1,16 @@
+import { Sequelize, DataTypes, Model } from "sequelize";
+
+const sequelize = new Sequelize(); //url de nuestra db
+//const sequelize = new Sequelize("process.env.DB_URI");
+
+async function authenticate() {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+authenticate();
+export default sequelize;
+export { DataTypes, Model };
